bb650c50015049dbf67060823872e1e5d6c40450
Rafi <mr.k779@outlook.com>
2025-12-02 03:25:30 -0500

    Create opengraph-image.tsx

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,22 @@
+import { generate as DefaultImage } from "fumadocs-ui/og";
+import { ImageResponse } from "next/og";
+
+export const alt = "Imgforge";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default async function Image() {
+  return new ImageResponse(
+    <DefaultImage
+      title="Imgforge"
+      description="Image Processing Reimagined in Rust"
+      site="Imgforge"
+    />,
+    {
+      ...size,
+    },
+  );
+}
